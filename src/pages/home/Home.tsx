51ef6e867e74705738a869e0f1327a6edc611bf4
najim2004 <najim10112004@gmail.com>
2024-09-26 17:03:23 +0600

    update:login and signup page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="flex justify-between items-center">
       <h3>This is home</h3>
       <Link to={"/blogs"}>Blogs</Link>
+      <Link to={"/signup"}>SignUp</Link>
+      <Link to={"/signin"}>SignIn</Link>
     </div>
   );
 };
